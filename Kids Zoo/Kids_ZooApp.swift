706a122fc45757e08259239f8adbe277f79a40e9
Mahmoud Abdelhady <eng.mahmoud.sh.77@gmail.com>
2024-10-23 15:03:42 +0300

    Initial Commit

---
+++ Kids Zoo/Kids_ZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_ZooApp.swift
+//  Kids Zoo
+//
+//  Created by Mahmoud Elsharkawy on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kids_ZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
